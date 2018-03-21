a88533d7ce0db9aa5da932f00a6d7a51d67a8ba9
Patrick McGrath <mcgrath.p@husky.neu.edu>
2018-03-20 22:53:54 -0400

    Static event page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "homepage#index"
 
-  resources :events, path: "events", except: [:show]
+  #resources :events, path: "events", except: [:show]
   resources :donate, only: [:index]
 
   get "about", to: "pages#show", page: "about"
   get "story", to: "pages#show", page: "story"
+  get "events", to: "pages#show", page: "events"
 
   Rails.application.routes.draw do
    get "pages/:page" => "pages#show"
