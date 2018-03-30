d23381d6171508439d44827bb21f6beaecbc9c60
Patrick McGrath <mcgrath.p@husky.neu.edu>
2018-03-30 16:18:05 -0400

    Removing turbolinks, fixing carousel issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,4 @@
 //= require rails-ujs
 //= require jquery
 //= require jquery.slick
-//= require turbolinks
 //= require_tree .
--- app/assets/javascripts/shared/image-carousels.js
+++ app/assets/javascripts/shared/image-carousels.js
@@ -8,3 +8,4 @@ $(document).ready(function() {
     arrows: false
   });
 })
+console.log("here");
