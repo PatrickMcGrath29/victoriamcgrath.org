7ec74882a0ec8f54382b8d684f058c1ce16fcff5
Patrick McGrath <mcgrath.p@husky.neu.edu>
2018-04-06 17:38:09 -0400

    Fixing JS listeners, to use turbolinks and not ready

--- app/assets/javascripts/shared/image-carousels.js
+++ app/assets/javascripts/shared/image-carousels.js
@@ -1,4 +1,4 @@
-$(document).on("ready turbolinks:load", function() {
+$(document).on("turbolinks:load", function() {
   $(".carousel").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -9,7 +9,7 @@ $(document).on("ready turbolinks:load", function() {
   });
 });
 
-$(document).on("ready turbolinks:load", function() {
+$(document).on("turbolinks:load", function() {
   $(".2-wrap-carousel").slick({
     slidesToShow: 2,
     slidesToScroll: 1,
@@ -20,7 +20,7 @@ $(document).on("ready turbolinks:load", function() {
   });
 })
 
-$(document).on("ready turbolinks:load", function() {
+$(document).on("turbolinks:load", function() {
   $(".3-wrap-carousel").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
--- app/assets/javascripts/shared/navigation.js
+++ app/assets/javascripts/shared/navigation.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).on("turbolinks:load", function() {
   $(".navbar-collapse-toggle").click(function() {
     $(this).toggleClass("navbar-active");
     $("nav.navbar").toggleClass("mobile-hidden");
   });
-})
+});
